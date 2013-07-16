75b2f50c80b084b052f6f136e37b5cdc3675852e
Justin Shacklette <justin@saturnboy.com>
2013-07-16 10:06:08 -0600

    added lesson 4

---
+++ CollView/CollView/ViewController4.h
@@ -0,0 +1,15 @@
+//
+//  ViewController4.h
+//  CollView
+//
+//  Created by Justin on 7/9/13.
+//  Copyright (c) 2013 Saturnboy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController4 : UIViewController <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (nonatomic, strong) IBOutlet UICollectionView *collectionView;
+
+@end
---
+++ CollView/CollView/ViewController4.m
@@ -0,0 +1,64 @@
+//
+//  ViewController4.m
+//  CollView
+//
+//  Created by Justin on 7/9/13.
+//  Copyright (c) 2013 Saturnboy. All rights reserved.
+//
+
+#import "ViewController4.h"
+#import "People.h"
+
+@interface ViewController4 ()
+
+@end
+
+@implementation ViewController4
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Collection view data source
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView*)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(UICollectionView*)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [[[People sharedInstance] people] count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView
+                  cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [self.collectionView dequeueReusableCellWithReuseIdentifier:@"MyCell" forIndexPath:indexPath];
+    
+    NSLog(@"%d : view=%.0fx%.0f cell=%.0fx%.0f",
+          indexPath.item,
+          self.collectionView.frame.size.width, self.collectionView.frame.size.height,
+          cell.frame.size.width, cell.frame.size.height);
+    
+    Person *p = [[People sharedInstance] person:indexPath.item];
+    UILabel* lbl = (UILabel *)[cell viewWithTag:123];
+    lbl.text = [p.name stringByReplacingOccurrencesOfString:@" " withString:@"\n" options:0 range:NSMakeRange(0, p.name.length)];
+    [cell addSubview:lbl];
+    
+    return cell;
+}
+
+@end
